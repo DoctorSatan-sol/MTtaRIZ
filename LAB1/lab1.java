0ca446ae11425530970cac250ca2fac154348664
DoctorSatan <ashorteim5@gmail.com>
2025-10-01 11:35:23 +0300

    test1235

--- LAB1/lab1.java
+++ LAB1/lab1.java
@@ -2,5 +2,6 @@ class Lab1 {
     public static void main(String[] args) {
         System.out.println("Its works");
         System.out.println("Hello World 123");
+        System.out.println("test");
         }
 }
